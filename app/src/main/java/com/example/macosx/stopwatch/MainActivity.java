56b2287c94069215475f8cb2e4951b90ca74ae7d
HaidarRais <zwingfame@gmail.com>
2018-03-13 14:40:55 +0700

    Finish
    FInish

---
+++ app/src/main/java/com/example/macosx/stopwatch/MainActivity.java
@@ -0,0 +1,153 @@
+package com.example.macosx.stopwatch;
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView ;
+    ImageButton start,pause,reset,lap;
+
+    long MillisecondTime, StartTime, TimeBuff, UpdateTime = 0L ;
+
+    Handler handler;
+
+    int Seconds, Minutes, MilliSeconds ;
+
+    ListView listView ;
+
+    String[] ListElements = new String[] {  };
+
+    List<String> ListElementsArrayList ;
+
+    ArrayAdapter<String> adapter ;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView)findViewById(R.id.textView);
+        start = (ImageButton)findViewById(R.id.button);
+        pause = (ImageButton)findViewById(R.id.button2);
+        reset = (ImageButton)findViewById(R.id.button3);
+        lap = (ImageButton)findViewById(R.id.button4) ;
+        listView = (ListView)findViewById(R.id.listview1);
+
+        handler = new Handler() ;
+
+        ListElementsArrayList = new ArrayList<String>(Arrays.asList(ListElements));
+
+        adapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1,
+                ListElementsArrayList
+        );
+
+        listView.setAdapter(adapter);
+
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                StartTime = SystemClock.uptimeMillis();
+                handler.postDelayed(runnable, 0);
+
+                reset.setEnabled(false);
+                reset.setVisibility(View.INVISIBLE);
+
+
+            }
+        });
+
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                TimeBuff += MillisecondTime;
+
+                handler.removeCallbacks(runnable);
+
+                reset.setEnabled(true);
+                reset.setVisibility(View.VISIBLE);
+
+
+            }
+        });
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                MillisecondTime = 0L ;
+                StartTime = 0L ;
+                TimeBuff = 0L ;
+                UpdateTime = 0L ;
+                Seconds = 0 ;
+                Minutes = 0 ;
+                MilliSeconds = 0 ;
+
+                textView.setText("00:00:00");
+
+                ListElementsArrayList.clear();
+
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        lap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                ListElementsArrayList.add(textView.getText().toString());
+
+                adapter.notifyDataSetChanged();
+
+            }
+        });
+
+    }
+
+    public Runnable runnable = new Runnable() {
+
+        public void run() {
+
+            MillisecondTime = SystemClock.uptimeMillis() - StartTime;
+
+            UpdateTime = TimeBuff + MillisecondTime;
+
+            Seconds = (int) (UpdateTime / 1000);
+
+            Minutes = Seconds / 60;
+
+            Seconds = Seconds % 60;
+
+            MilliSeconds = (int) (UpdateTime % 1000);
+
+            textView.setText("" + Minutes + ":"
+                    + String.format("%02d", Seconds) + ":"
+                    + String.format("%03d", MilliSeconds));
+
+            handler.postDelayed(this, 0);
+        }
+
+    };
+
+}
